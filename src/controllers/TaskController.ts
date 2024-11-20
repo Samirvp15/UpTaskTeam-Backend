6cb8ea6fed10d6f6c3ae6385b9e087d63765cc32
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-19 23:55:04 -0500

    add TaskController with NestedResourceRouting

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,37 @@
+import type { Request, Response } from 'express'
+import Project from '../models/Project'
+import Task from '../models/Task'
+
+
+export class TaskController {
+
+    static createTask= async (req: Request, res: Response) => {
+
+        const { projectId } = req.params
+        const project = await Project.findById(projectId)
+        
+        if (!project) {
+            const error = new Error('Proyecto no encontrado')
+            res.status(404).json({ error: error.message })
+            return
+        }
+        
+        try {
+            const task = new Task(req.body)
+            // ASIGNAR PROYECTO A TAREA
+            task.project = project.id
+           // AGREGAR UNA TAREA O ARRAY DE TAREAS A PROYECTO
+            project.tasks.push(task.id)
+            await task.save()
+            await project.save()
+            res.send('Tarea creada correctamente')
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error en el Servidor' })
+        }
+
+
+    }
+}
+
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { body, param } from 'express-validator'
 import { ProjectController } from "../controllers/ProjectController";
 import { handleInputErrors } from "../middlewares/validation";
+import { TaskController } from "../controllers/TaskController";
 
 const router = Router()
 
@@ -40,6 +41,13 @@ router.delete('/:id',
     param('id').isMongoId().withMessage('ID no valido'),
     handleInputErrors,
     ProjectController.deleteProject)
+
+
+// ROUTES FOR TASKS
+
+router.post('/:projectId/tasks',
+    TaskController.createTask
+)
 
 export default router
 
