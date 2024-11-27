1b0ad1e75e3ee60a34be4870479ac46435fec866
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-27 15:02:10 -0500

    add morgan for logging

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db'
 import cors from 'cors'
+import morgan from 'morgan'
 import projectRoutes from './routes/projectRoutes'
 import { corsConfig } from './config/cors'
  
@@ -12,6 +13,8 @@ connectDB()
 
 const server = express()
 
+//Logging
+server.use(morgan('dev'))
 //Middlewares
 server.use(cors(corsConfig))
 server.use(express.json())
